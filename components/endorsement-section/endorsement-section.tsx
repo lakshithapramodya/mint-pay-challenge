84d056cb158053b58b506669c53fdf0f8a390552
Lakshitha Pramodya <pramodya8@gmail.com>
2023-10-22 21:25:10 +0530

    small fix

--- components/endorsement-section/endorsement-section.tsx
+++ components/endorsement-section/endorsement-section.tsx
@@ -4,7 +4,7 @@ import EndorsementCard from "./endorsement-card";
 
 const EndorsementSection = () => {
   return (
-    <section className="px-4 pb-9 md:px-[47px] xl:px-24">
+    <section className="px-4 pb-9 md:px-[47px] xl:px-24 xl:pb-24">
       <div className="grid md:grid-cols-2 md:gap-x-8 xl:grid-cols-3 [&>*:nth-child(2n+1)]:hidden xl:[&>*:nth-child(2n+1)]:block md:[&>*:nth-child(3)]:block">
         {endorsementData.map((item) => (
           <EndorsementCard key={item.id} item={item} />
