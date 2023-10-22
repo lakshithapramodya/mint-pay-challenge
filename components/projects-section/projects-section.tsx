a39836e25aca393430fc653347f09d9ae3c6d01d
Lakshitha Pramodya <pramodya8@gmail.com>
2023-10-22 18:01:46 +0530

    small fix

--- components/projects-section/projects-section.tsx
+++ components/projects-section/projects-section.tsx
@@ -4,13 +4,13 @@ import ProjectCard from "./project-card";
 
 const ProjectsSection = () => {
   return (
-    <section className="space-y-3 px-4 pb-9 md:px-[47px] xl:px-24">
+    <section className="space-y-3 px-4 pb-9 md:px-[47px] xl:space-y-9 xl:px-24">
       <div className="grid gap-3 md:grid-cols-2 xl:grid-cols-3 xl:gap-9 [&>*:nth-child(n+4)]:hidden md:[&>*:nth-child(n+4)]:grid md:[&>*:nth-child(n+5)]:hidden xl:[&>*:nth-child(n+5)]:grid">
         {projectsData.map((project) => (
           <ProjectCard key={project.id} data={project} />
         ))}
       </div>
-      <div className="w-full">
+      <div className="relative z-10 w-full">
         <button className="border-primary m-auto flex items-start rounded-[23px] border border-solid px-6 py-2">
           Load more
         </button>
