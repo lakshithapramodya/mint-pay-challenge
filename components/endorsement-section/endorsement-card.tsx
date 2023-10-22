0389215f4628bc1fd2eca6e32c4befb21021dce3
Lakshitha Pramodya <pramodya8@gmail.com>
2023-10-22 21:07:04 +0530

    endorsement section refcator

---
+++ components/endorsement-section/endorsement-card.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+interface EndorsementCardProps {
+  item: {
+    id: number;
+    position: string;
+    endorsements: {
+      id: number;
+      name: string;
+      image: string;
+      endorse: string;
+    }[];
+  };
+}
+
+const EndorsementCard: React.FC<EndorsementCardProps> = ({ item }) => {
+  return (
+    <div className="space-y-8 md:space-y-9">
+      {item.endorsements.map((data) => (
+        <div
+          key={data.id}
+          className="relative m-auto max-w-full space-y-6 rounded-[23px] border border-solid border-neutral-200 p-6"
+        >
+          {data.id == 2 && (
+            <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-t from-white to-transparent " />
+          )}
+          <div className="flex items-center space-x-4">
+            <Image
+              className="h-14 w-14 shrink-0 object-contain"
+              src={data.image}
+              alt={data.name}
+              width="100"
+              height="100"
+            />
+            <div className="space-y-[2px]">
+              <h4 className="text-primaryText self-stretch text-base leading-[150%]">
+                {data.name}
+              </h4>
+              <span className="text-primary line-clamp-1 h-6 self-stretch text-sm leading-[150%]">
+                {item.position}
+              </span>
+            </div>
+          </div>
+          <p className="text-primaryText self-stretch text-sm leading-[160%]">
+            {data.endorse}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EndorsementCard;
