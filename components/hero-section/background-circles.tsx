e7b5a094525203717a92d4906d6aa907288910d4
Lakshitha Pramodya <pramodya8@gmail.com>
2023-10-22 11:39:25 +0530

    background circles refactor

--- components/hero-section/background-circles.tsx
+++ components/hero-section/background-circles.tsx
@@ -1,17 +1,53 @@
-const BackgroundCircles = () => {
+import { cn } from "@/lib/utils";
+
+interface BackgroundCirclesProps {
+  className?: string;
+}
+
+const BackgroundCircles: React.FC<BackgroundCirclesProps> = ({ className }) => {
   return (
     <div className="relative flex items-center justify-center">
-      <div className="background-circles h-[85px] w-[85px] xl:h-[133px] xl:w-[132px]" />
+      <div
+        className={cn(
+          "background-circles h-[85px] w-[85px] xl:h-[133px] xl:w-[132px]",
+          className,
+        )}
+      />
 
-      <div className="background-circles h-[170px] w-[170px] opacity-50 xl:h-[262px] xl:w-[261px]" />
+      <div
+        className={cn(
+          "background-circles h-[170px] w-[170px] opacity-50 xl:h-[262px] xl:w-[261px]",
+          className,
+        )}
+      />
 
-      <div className="background-circles h-[256px] w-[256px] opacity-40 xl:h-[395px] xl:w-[395px]" />
+      <div
+        className={cn(
+          "background-circles h-[256px] w-[256px] opacity-40 xl:h-[395px] xl:w-[395px]",
+          className,
+        )}
+      />
 
-      <div className="background-circles h-[355px] w-[355px] opacity-30 xl:h-[545px] xl:w-[545px]" />
+      <div
+        className={cn(
+          "background-circles h-[355px] w-[355px] opacity-30 xl:h-[545px] xl:w-[545px]",
+          className,
+        )}
+      />
 
-      <div className="background-circles  h-[445px] w-[445px] opacity-20 xl:h-[685px] xl:w-[685px]" />
+      <div
+        className={cn(
+          "background-circles  h-[445px] w-[445px] opacity-20 xl:h-[685px] xl:w-[685px]",
+          className,
+        )}
+      />
 
-      <div className="background-circles  h-[550px] w-[550px] opacity-10 xl:h-[855px] xl:w-[845px]" />
+      <div
+        className={cn(
+          "background-circles  h-[550px] w-[550px] opacity-10 xl:h-[855px] xl:w-[845px]",
+          className,
+        )}
+      />
     </div>
   );
 };
--- components/hero-section/hero-section.tsx
+++ components/hero-section/hero-section.tsx
@@ -5,7 +5,7 @@ import HeroHeader from "./hero-header";
 const HeroSection = () => {
   return (
     <section className="overflow-hidden px-4 pb-14 pt-12 md:px-[47px] xl:px-24">
-      <BackgroundCircles />
+      <BackgroundCircles className="ml-[calc(100%+50px)] mt-[250px] md:ml-[calc(100%+100px)] xl:ml-[calc(100%+200px)] xl:mt-40" />
 
       <div className="flex flex-col space-y-8 xl:flex-row xl:items-center xl:justify-between">
         <HeroHeader />
